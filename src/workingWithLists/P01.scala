7648fc6f6b443d9063a42d5c9b926ff28b2f880c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-01 12:38:59 +0200

    P01, find the last element of a list

---
+++ src/workingWithLists/P01.scala
@@ -0,0 +1,18 @@
+package workingWithLists
+// Find the last element of a list.
+object P01 extends App {
+  // Built in solutions.
+  val list = List(1, 2, 3, 4, 5)
+  println(list.last)
+  println(list.reverse.head) // Why not.
+  println(list.foldLeft(0)((_, current) => current))
+  println(list.reduce((_, current) => current)) // More elegant. Also, n - 1 operations compared to foldLeft.
+  println(list.foreach(x => if (x == list.last) println(x))) // 😎
+
+  // Find the end of the list recursively.
+  def last[A](list: List[A]): A = { // O(n) time complexity.
+    if (list.tail.isEmpty) list.head
+    else last(list.tail)
+  }
+  println(last(List(1, 2, "Hello", 3, 'a')))
+}

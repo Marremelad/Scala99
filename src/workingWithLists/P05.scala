25a3fb30554c689fc100a258389627f08d495519
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-04 13:20:26 +0200

    P05, reverse a list

---
+++ src/workingWithLists/P05.scala
@@ -0,0 +1,21 @@
+package workingWithLists
+
+import scala.annotation.tailrec
+
+// Reverse a list.
+object P05 extends App {
+  def reverse[A](list: List[A]): List[A] =
+    if (list.isEmpty) list
+    else reverse(list.tail) ++ List(list.head) // O(n²)
+
+  def reverseTailRec[A](list: List[A]): List[A] = {
+    @tailrec
+    def aux(remaining: List[A], acc: List[A] = Nil): List[A] =
+      if (remaining.isEmpty) acc
+      else aux(remaining.tail, remaining.head :: acc) // O(n) because of :: (prepend) instead of ++.
+
+    aux(list)
+  }
+  println(reverse(List(1, 2, 3)))
+  println(reverseTailRec(List(1, 2, 3)))
+}

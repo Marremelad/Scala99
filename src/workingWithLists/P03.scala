7a6f594a36faf9474d172baa9d3ad5b2811c4cc1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-04 12:12:06 +0200

    P03, find the Kth element of a list

---
+++ src/workingWithLists/P03.scala
@@ -0,0 +1,14 @@
+package workingWithLists
+// Find the Kth element in a list.
+object P03 extends App {
+
+  // Find Kth element recursively.
+  def nth[A](n: Int, list: List[A]): A =
+    if (n <= 0) {
+      if (list.isEmpty) throw new NoSuchElementException
+      else list.head
+    }
+    else nth(n - 1, list.tail)
+
+  println(nth(2, List(1, 2, 3)))
+}

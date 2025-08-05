f10b6a0232d580d8ee00b54aca19d7a24352f478
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-05 10:55:23 +0200

    P04, find the number of elements of a list

--- src/workingWithLists/P04.scala
+++ src/workingWithLists/P04.scala
@@ -1,7 +1,6 @@
 package workingWithLists
 // Find the number of elements of a list.
 object P04 extends App {
-
   def length[A](list: List[A], acc: Int = 0): Int =
     if (list.isEmpty) acc
     else length(list.tail, 1 + acc)

ca8f4be69106fce7ec0dba422436622a97be28b2
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:45:44 +0200

    P07, flatten a nested list structure

--- src/workingWithLists/P07.scala
+++ src/workingWithLists/P07.scala
@@ -1,5 +1,10 @@
 package workingWithLists
 // Flatten a nested list structure.
+
+//Example:
+//
+//  scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+//res0: List[Any] = List(1, 1, 2, 3, 5, 8)
 object P07 extends App {
   val list = List(List(1, List(1)), 2, List(3, List(5, 8)))
 

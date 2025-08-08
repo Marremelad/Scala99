4337a17bb30fa0ecd225b544a77be2c3180af0f0
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:45:03 +0200

    P06, find out if a list is a palindrome

--- src/workingWithLists/P06.scala
+++ src/workingWithLists/P06.scala
@@ -1,5 +1,10 @@
 package workingWithLists
 // Find out if a list is a palindrome.
+
+//Example:
+//
+//  scala> isPalindrome(List(1, 2, 3, 2, 1))
+//res0: Boolean = true
 object P06 extends App {
   def isPalindrome[A](list: List[A]): Boolean = {
     list == P05.reverseTailRec(list) // Use the reverse function from previous problem.

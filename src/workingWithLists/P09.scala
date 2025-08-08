214901e8437625646c3b455847eee13007a5e4a2
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 10:26:52 +0200

    P09, pack consecutive duplicates of elements into sublists

---
+++ src/workingWithLists/P09.scala
@@ -0,0 +1,21 @@
+package workingWithLists
+
+import scala.annotation.tailrec
+
+// Pack consecutive duplicates of list elements into sublists.
+object P09 extends App {
+  def pack[A](list: List[A]): List[List[A]] = {
+    @tailrec
+    def go(list: List[A], sublist: List[A], result: List[List[A]]): List[List[A]] = {
+      list match {
+        case Nil => (sublist :: result).reverse
+        case head :: tail =>
+          if (sublist.head == head) go(tail, head :: sublist, result)
+          else go(tail, head :: Nil, sublist :: result)
+      }
+    }
+    go(list, List(list.head), Nil)
+  }
+
+  println(pack(List('a', 'a', 'a', 'b', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')))
+}

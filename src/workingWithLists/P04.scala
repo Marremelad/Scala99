a4946602fe5b18fac02eeeb64ee15cb763d3e8d1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:41:45 +0200

    P04, find the number of elements of a list

--- src/workingWithLists/P04.scala
+++ src/workingWithLists/P04.scala
@@ -1,6 +1,14 @@
 package workingWithLists
+
+import scala.annotation.tailrec
 // Find the number of elements of a list.
+
+//Example:
+//
+//  scala> length(List(1, 1, 2, 3, 5, 8))
+//res0: Int = 6
 object P04 extends App {
+  @tailrec
   def length[A](list: List[A], acc: Int = 0): Int =
     if (list.isEmpty) acc
     else length(list.tail, 1 + acc)

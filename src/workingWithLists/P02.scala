3e2d0443f6a2b106fea83486bcb314e0536344c3
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:39:25 +0200

    P02, find the last but one element of a list

--- src/workingWithLists/P02.scala
+++ src/workingWithLists/P02.scala
@@ -1,5 +1,11 @@
 package workingWithLists
 // Find the last but one element of a list.
+
+//Example:
+//
+//  scala> penultimate(List(1, 1, 2, 3, 5, 8))
+//res0: Int = 5
+
 object P02 extends App {
   // Built in solutions.
 

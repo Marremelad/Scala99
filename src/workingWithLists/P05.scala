4469de4651b71fb8389948a017ea331389212591
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:44:34 +0200

    P05, reverse a list

--- src/workingWithLists/P05.scala
+++ src/workingWithLists/P05.scala
@@ -1,8 +1,12 @@
 package workingWithLists
 
 import scala.annotation.tailrec
-
 // Reverse a list.
+
+//Example:
+//
+//  scala> reverse(List(1, 1, 2, 3, 5, 8))
+//res0: List[Int] = List(8, 5, 3, 2, 1, 1)
 object P05 extends App {
   def reverse[A](list: List[A]): List[A] =
     if (list.isEmpty) list

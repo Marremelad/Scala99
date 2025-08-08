864a517ade3220cefcbc6ff4242478c8722062f3
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:40:35 +0200

    P03, find the Kth element of a list

--- src/workingWithLists/P03.scala
+++ src/workingWithLists/P03.scala
@@ -1,7 +1,18 @@
 package workingWithLists
+
+import scala.annotation.tailrec
 // Find the Kth element in a list.
+
+//By convention, the first element in the list is element 0.
+//
+//Example:
+//
+//  scala> nth(2, List(1, 1, 2, 3, 5, 8))
+//res0: Int = 2
+
 object P03 extends App {
   // Find Kth element recursively.
+  @tailrec
   def nth[A](n: Int, list: List[A]): A =
     if (n <= 0) {
       if (list.isEmpty) throw new NoSuchElementException

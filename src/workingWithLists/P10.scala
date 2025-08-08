9122fef95f5313719632208d8ead78fbb9cab598
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-08 11:08:00 +0200

    P10, run-length encoding of a list

--- src/workingWithLists/P10.scala
+++ src/workingWithLists/P10.scala
@@ -4,6 +4,6 @@ object P10 extends App {
   def encode[A](list: List[A]): List[(Int, A)] = {
     P09.pack(list).map(x => (x.length, x.head))
   }
-  println(encode(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')))
 
+  println(encode(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')))
 }

5100b08b3e51b8e1072a37636afca4b08de88fb6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-14 09:54:13 +0200

    P20, remove the Kth element from a list

---
+++ src/workingWithLists/P20.scala
@@ -0,0 +1,28 @@
+package workingWithLists
+
+import scala.annotation.tailrec
+// Remove the Kth element from a list
+
+//Example:
+//
+//  scala> removeAt(1, List('a', 'b', 'c', 'd'))
+//res0: (List[Char], Char) = (List('a', 'c', 'd'),'b')
+
+object P20 extends App {
+  def removeAt[A](n: Int, list: List[A]): Option[(List[A], A)] = {
+    @tailrec
+    def go(index: Int, remaining: List[A], acc: List[A]): (List[A], A) =
+      remaining match {
+        case head :: tail =>
+          if (index == n) (acc.reverse ::: tail, head)
+          else go(index + 1, tail, head :: acc)
+      }
+
+    if (n < 0 || n >= list.length) None
+    else Some(go(0, list, Nil))
+  }
+
+  val someValue = removeAt(100, List('a', 'b', 'c', 'd')).getOrElse(None)
+  println(someValue)
+}
+

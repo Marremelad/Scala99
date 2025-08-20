2e2707b8f7185de1110adcb38af2ba5d901f2f9a
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-20 18:29:12 +0200

    P21, insert an element at a given position into a list

--- src/workingWithLists/P21.scala
+++ src/workingWithLists/P21.scala
@@ -1,5 +1,23 @@
 package workingWithLists
+// Insert an element at a given position into a list.
 
-object P21 {
+//Example:
+//
+//  scala> insertAt("new", 1, List('a, 'b, 'c, 'd))
+//res0: List[Any] = List('a', "new", 'b', 'c', 'd')
 
+object P21 extends App {
+  def insert[A](element: A, n: Int, list: List[A]): List[A] = {
+    def go(index: Int, list: List[A], acc: List[A]): List[A] = {
+      list match {
+        case Nil => (element :: acc).reverse
+        case head :: tail =>
+          if (index == n) acc.reverse ::: element :: head :: tail
+          else go(index + 1, tail, head :: acc)
+      }
+    }
+    go(0, list, Nil)
+  }
+
+  println(insert('A', 2, List(1, 2, 3, 4, 5)))
 }
